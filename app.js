beb10d319344289f7d771a3cc1a7de6425730668
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2020-03-31 12:31:18 -0400

    Colocando nossa aplicação pra rodar no navegador

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.get('/',(req,res,next) =>{
+    res.send({
+        title:'Meu primeiro servidor com Express',
+        version: '0.0.0'
+    })
+})
+app.listen(port,err =>{
+    console.log(`Server is listening on ${port}`)
+})
