4b5b51988cc3ba567c48c705590c4890b42a310c
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2020-03-31 20:05:44 -0400

    Curriculo modificado para meu usuario

--- app.js
+++ app.js
@@ -13,6 +13,33 @@ app.get('/',function(req,res,next){
         version: '0.0.0'
     })
 })
+app.get('/curriculo',function(req,res,next){
+    res.render('curriculo',{
+        title: 'Meu Curriculo',
+        name: 'Roberto dos Santos Filho',
+        profession: 'Apreendendo Programação Web',
+        description: 'Estudando linguagem de programação web, como Html, Css, JavaScript, NodeJs, MongoDb, ReactJs e ReactNative.',
+        experience:[
+            {
+                company: 'Curso Sistema de Informação',
+                office: 'Desenvolvimento FullStack',
+                description:'Curso de Programação Web'
+            },
+            {
+                company:'Youtube',
+                office: 'Auto Apreendizado',
+                description:'Assistir videos para apreender e ter conhecimento de uma determinada linguagem.'
+            }
+        ],
+        education:[
+            {
+                institution: 'Faculdade Unemat',
+                description: '3° semestre do curso superior de sistema de informação.'
+            }
+        ],
+        skills:['Backend','Frontend','Banco de Dados','Mobile']
+    })
+})
 app.listen(port,err =>{
     console.log(`Server is listening on ${port}`)
 })
